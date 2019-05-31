9369124b89ef55ad449690035c56d306c10f0a11
pritpambhar <pritpambhar@gmail.com>
2019-05-31 19:07:03 +0530

    python file upload

---
+++ test.py
@@ -0,0 +1,23 @@
+import mysql.connector as mc
+try:
+
+	mydb=mc.connect(
+		host="localhost",
+		user="root",
+		passwd="",
+		database="mygst"
+	)
+
+	mycursor=mydb.cursor();
+
+	mycursor.execute("select * from users");
+
+	result=mycursor.fetchall();
+
+	for x in result:
+		print(x)
+
+except:
+	print("error");
+#print(mydb)
+
